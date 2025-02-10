82d432d2ba6a0f8d00f859ba29db1e82d6d682c1
Mason Ritchason <masonritchason@gmail.com>
2025-02-10 16:17:27 -0500

    feat: begin implementing QRCode class

---
+++ LotCoMPrinter/Models/QRCode.cs
@@ -0,0 +1,10 @@
+using QRCoder;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using SixLabors.ImageSharp.Processing;
+
+namespace LotCoMPrinter.Models;
+
+public class QRCode {
+
+}
