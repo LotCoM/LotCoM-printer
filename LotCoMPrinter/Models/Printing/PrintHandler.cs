f7adee57591669bd5ca67cce5eeed801428ab934
Mason Ritchason <masonritchason@gmail.com>
2025-02-12 15:09:30 -0500

    feat: initial PrintHandler class implementation

---
+++ LotCoMPrinter/Models/Printing/PrintHandler.cs
@@ -0,0 +1,11 @@
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace LotCoMPrinter.Models.Printing;
+
+public static class PrintHandler {
+
+    public static void PrintLabel(Image<Rgba32> LabelImage) {
+
+    }
+}
