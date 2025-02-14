59eac348ab79d481375a8ddc48f3684a917cf9a5
Mason Ritchason <masonritchason@gmail.com>
2025-02-14 09:36:47 -0500

    refactor(ModelData): tweak ModelData datasource to work with new part sourcing method

--- LotCoMPrinter/Models/Datasources/ModelData.cs
+++ LotCoMPrinter/Models/Datasources/ModelData.cs
@@ -5,17 +5,16 @@ namespace LotCoMPrinter.Models.Datasources;
 /// </summary>
 public static class ModelData {
     // Model Data masterlist
-    private static readonly Dictionary<string, List<Dictionary<string, string>>> _modelsMasterList = new Dictionary<string, List<Dictionary<string, string>>> {
-        {"T20", [
-            new Dictionary<string, string> {{"00-T20-532AP-A000-YB1", "HOUSING, PIVOT*"}}
-        ]}
-    };
-    public static Dictionary<string, List<Dictionary<string, string>>> ModelsMasterList {
+    private static readonly List<string> _modelsMasterList = [
+        "T20", "TBA", "TLA"
+    ];
+    public static List<string> ModelsMasterList {
         get {return _modelsMasterList;}
     }
 
     /// <summary>
-    /// Attempts to find the Model Number attached to PartNumber in the Models masterlist.
+    /// Attempts to find the Model Number attached to PartNumber. 
+    /// Only returns a number if the Model is in the Models masterlist.
     /// </summary>
     /// <param name="PartNumber">The Part Number to imply the Model Number from.</param>
     /// <returns>string Model Number (if found).</returns>
@@ -25,7 +24,7 @@ public static class ModelData {
             // split the Model Number out of the Part Number (xx-[MODELNUMBER]-xxx-xxx...)
             string ModelNumber = PartNumber.Split("-")[1];
             // try to find that Model in the Models masterlist
-            if (ModelsMasterList.ContainsKey(ModelNumber)) {
+            if (ModelsMasterList.Contains(ModelNumber)) {
                 return ModelNumber;
             // Model is not in the Keys list; doesn't exist
             } else {
