fb1f092922f055c31cd584149d17fd58c05a863c
Mason Ritchason <masonritchason@gmail.com>
2025-02-20 13:38:56 -0500

    refactor!(label printing): add queue consumption logic to the LabelPrintJob class

--- LotCoMPrinter/Models/Printing/LabelPrintJob.cs
+++ LotCoMPrinter/Models/Printing/LabelPrintJob.cs
@@ -1,4 +1,5 @@
 using System.Drawing;
+using LotCoMPrinter.Models.Datasources;
 using LotCoMPrinter.Models.Exceptions;
 using LotCoMPrinter.Models.Labels;
 
@@ -8,12 +9,14 @@ namespace LotCoMPrinter.Models.Printing;
 /// Creates a Print Job that can generate a Bitmap image Label and spool a print job to the printing system.
 /// </summary>
 /// <param name="LabelInformation">The Data to be encoded in the Label's QR Code and shown on the Label itself (a validated UI Capture from InterfaceCaptureValidator).</param>
-public class LabelPrintJob(List<string> LabelInformation) {
+public class LabelPrintJob(List<string> LabelInformation, string SerializeMode, string ModelNumber) {
     // private class properties to hold Label data and generated Label Bitmap
     private List<string> _labelInformation = LabelInformation;
     // split out the JBK # value to apply as the header
     private string _header = LabelInformation[3].Split(":")[1].Replace(" ", "");
     private Bitmap? _label = null;
+    private string _serializeMode = SerializeMode;
+    private string _modelNumber = ModelNumber;
 
     /// <summary>
     /// Creates a Label object and Bitmap image from the Job's saved information. Stores the generated Bitmap image in _label property.
@@ -41,14 +44,26 @@ public class LabelPrintJob(List<string> LabelInformation) {
         // create a PrintHandler object for the new Label
         if (_label != null) {
             PrintHandler LabelPrinter = new PrintHandler(_label);
+            // try to print the Label
+            bool Printed;
             try {
                 await LabelPrinter.PrintLabelAsync();
+                Printed = true;
             // handle errors thrown by the PrintLabelAsync() method
-            } catch (AggregateException _ex){
+            } catch (Exception _ex){
                 App.AlertSvc!.ShowAlert(
                     "Failed to Print", "There was an error Printing the Label. Please see management to resolve this issue."
-                    + $"\n\nException Message(s): {_ex.InnerExceptions}"
+                    + $"\n\nException Message(s): {_ex.InnerException}"
                 );
+                Printed = false;
+            }
+            // consume the queued serializing number (only if the print was successful)
+            if (Printed) {
+                if (_serializeMode == "JBK") {
+                    await JBKQueue.ConsumeAsync(_modelNumber);
+                } else {
+                    await LotQueue.ConsumeAsync(_modelNumber);
+                }
             }
         }
     }
