ca07d1d20449ff06bbaf246a9530ac9dd968a59d
Mason Ritchason <masonritchason@gmail.com>
2025-02-28 13:11:28 -0500

    refactor(LabelPrintJob)!: remove serial assignment

--- LotCoMPrinter/Models/Printing/LabelPrintJob.cs
+++ LotCoMPrinter/Models/Printing/LabelPrintJob.cs
@@ -1,5 +1,5 @@
 using System.Drawing;
-using LotCoMPrinter.Models.Datasources;
+using LotCoMPrinter.Models.Serialization;
 using LotCoMPrinter.Models.Exceptions;
 using LotCoMPrinter.Models.Labels;
 
@@ -9,48 +9,14 @@ namespace LotCoMPrinter.Models.Printing;
 /// Creates a Print Job that can generate a Bitmap image Label and spool a print job to the printing system.
 /// </summary>
 /// <param name="LabelInformation">The Data to be encoded in the Label's QR Code and shown on the Label itself (a validated UI Capture from InterfaceCaptureValidator).</param>
-public class LabelPrintJob(List<string> LabelInformation, string SerializeMode, string ModelNumber, string LabelType) {
+/// <param name="LabelType">Either 'Full' or 'Partial' (from the UI BasketTypePicker control).</param>
+public class LabelPrintJob(List<string> LabelInformation, string LabelType) {
     // private class properties to hold Label data and generated Label Bitmap
     private List<string> _labelInformation = LabelInformation;
     // split out the JBK # value to apply as the header
     private string _header = LabelInformation[3].Split(":")[1].Replace(" ", "");
     private Bitmap? _label = null;
-    private string _serializeMode = SerializeMode;
-    private string _modelNumber = ModelNumber;
     private string _labelType = LabelType;
-
-    /// <summary>
-    /// Processes the use of a Serial Number for a Partial Label. 
-    /// Checks if there is a Serial Number cached for the Part Number.
-    /// If not, consumes and caches the queued Serial Number.
-    /// </summary>
-    /// <returns></returns>
-    private async Task<string?> ProcessSerialNumber() {
-        string? SerialNumber = null;
-        // run a new CPU thread to get the serial number for this label
-        await Task.Run(async () => {
-            // save the part number for later use
-            string PartNumber = _labelInformation[1].Split("\n")[0];
-            // check if there is already a cached number
-            SerialNumber = await SerialCache.FindNumberForPart(PartNumber);
-            // if there was no serial number found, consume and cache a new one
-            if (SerialNumber == null) {
-                if (_serializeMode == "JBK") {
-                    // consume the queued JBK number for this Model
-                    SerialNumber = await JBKQueue.ConsumeAsync(_modelNumber);
-                } else {
-                    // consume the queued Lot number for this Model and cache it under the part number
-                    SerialNumber = await LotQueue.ConsumeAsync(_modelNumber);
-                }
-                // if the label is a Partial, cache the Serial Number under the part number
-                if (_labelType == "Partial") {
-                    await SerialCache.CacheSerialNumber(SerialNumber, PartNumber);
-                }
-            }
-        });
-        // return the serial number
-        return SerialNumber;
-    }
 
     /// <summary>
     /// Creates a Label object and Bitmap image from the Job's saved information. Stores the generated Bitmap image in _label property.
@@ -58,13 +24,6 @@ public class LabelPrintJob(List<string> LabelInformation, string SerializeMode, 
     /// <returns></returns>
     private async Task GenerateLabelImage() {
         try {
-            // get the Label's Serial Number
-            string? SerialNumber = await ProcessSerialNumber();
-            if (SerialNumber == null) {
-                throw new LabelBuildException("Failed to assign a Serial Number to the Label");
-            }
-            // assign the Serial Number to the JBK/Lot # field in _labelInformation
-            _labelInformation[3] = SerialNumber;
             // full label
             if (_labelType == "Full") {
                 _label = await LabelGenerator.GenerateFullLabelAsync(_header, _labelInformation);
@@ -106,7 +65,6 @@ public class LabelPrintJob(List<string> LabelInformation, string SerializeMode, 
             }
             // consume the queued serializing number (only if the print was successful)
             if (Printed) {
-                await ProcessSerialNumber();
                 // remove the cached serial number here if the label was partial
                 if (_labelType == "Partial") {
                     await SerialCache.RemoveCachedSerialNumber(_labelInformation[3], _labelInformation[1].Split("\n")[0]);
