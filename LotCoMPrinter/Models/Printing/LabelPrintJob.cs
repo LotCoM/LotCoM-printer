4e4e7e73d2a7c1b4fa329049a4d5c161a2f6faef
Mason Ritchason <masonritchason@gmail.com>
2025-03-14 11:22:03 -0400

    refactor!(LabelPrintJob): replace alerts with exceptions
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Printing/LabelPrintJob.cs
+++ LotCoMPrinter/Models/Printing/LabelPrintJob.cs
@@ -22,6 +22,7 @@ public class LabelPrintJob(List<string> LabelInformation, string LabelType, stri
     /// Creates a Label object and Bitmap image from the Job's saved information. Stores the generated Bitmap image in _label property.
     /// </summary>
     /// <returns></returns>
+    /// <exception cref="LabelBuildException">Thrown if the LabelGenerator failed to create a Label.</exception>
     private async Task GenerateLabelImage() {
         // elicit the header (JBK # if serialization is JBK, MM/DD of date if serialization is Lot)
         try {
@@ -34,20 +35,24 @@ public class LabelPrintJob(List<string> LabelInformation, string LabelType, stri
             }
         // there was an unexpected error in the Label generation
         } catch (LabelBuildException _ex) {
-            App.AlertSvc!.ShowAlert(
-                "Failed to Print", "There was an error Printing the Label. Please see management to resolve this issue."
-                + $"\n\nException Message(s): {_ex.Message}"
-            );
+            throw new LabelBuildException(_ex.Message);
         }
     }
 
     /// <summary>
-    /// Runs the Print Job (creates a Handler for the Job and spools it to the system's printing system).
+    /// Runs the Print Job (creates a Handler for the Job and spools it to the OS' printing system).
     /// </summary>
     /// <returns></returns>
+    /// <exception cref="LabelBuildException">Thrown if the LabelGenerator failed to create a Label.</exception>
+    /// <exception cref="PrintRequestException">Thrown if there was an error communicating with the Printer or Printing System.</exception>
     public async Task<bool> Run() {
         // generate a Label from the saved Label information
-        await GenerateLabelImage();
+        try {
+            await GenerateLabelImage();
+        // there was an unexpected error in the label build; pass the error on
+        } catch (LabelBuildException _ex) {
+            throw new LabelBuildException(_ex.Message);
+        }
         // create a PrintHandler object for the new Label
         bool Printed = false;
         if (_label != null) {
@@ -57,11 +62,8 @@ public class LabelPrintJob(List<string> LabelInformation, string LabelType, stri
                 await LabelPrinter.PrintLabelAsync();
                 Printed = true;
             // handle errors thrown by the PrintLabelAsync() method
-            } catch (Exception _ex){
-                App.AlertSvc!.ShowAlert(
-                    "Failed to Print", "There was an error Printing the Label. Please see management to resolve this issue."
-                    + $"\n\nException Message(s): {_ex.InnerException}"
-                );
+            } catch (PrintRequestException _ex){
+                throw new PrintRequestException(_ex.Message);
             }
             // prepare the serial number and the serial cache for the end of the print job
             string SerialNumber = _labelInformation[3].Replace("JBK: ", "").Replace("Lot: ", "");
