c3241770370f769cb5e9034a6b60e7e04ee63bed
Mason Ritchason <masonritchason@gmail.com>
2025-03-14 12:42:25 -0400

    feat: implement ErrorPopup view
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Views/ErrorPopup.xaml.cs
@@ -0,0 +1,10 @@
+using CommunityToolkit.Maui.Views;
+
+namespace LotCoMPrinter.Views;
+
+public partial class ErrorPopup : Popup {
+    
+    public ErrorPopup() {
+        InitializeComponent();
+    }
+}
