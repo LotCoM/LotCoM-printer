b26aa750983954a5c85a4f1bb48aa8d8ec809225
Mason Ritchason <masonritchason@gmail.com>
2025-03-14 14:11:49 -0400

    feat(ErrorPopup): implement binding for popup custom text
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Views/ErrorPopup.xaml.cs
+++ LotCoMPrinter/Views/ErrorPopup.xaml.cs
@@ -3,8 +3,41 @@ using CommunityToolkit.Maui.Views;
 namespace LotCoMPrinter.Views;
 
 public partial class ErrorPopup : Popup {
+    // binding properties
+    private string _title = "";
+    /// <summary>
+    /// Serves the Title for the Popup.
+    /// </summary>
+    public string Title {
+        get {return _title;}
+        set {
+            OnPropertyChanged(_title);
+            OnPropertyChanged(Title);
+            _title = value;
+        }
+    }
+    private string _message = "";
+    /// <summary>
+    /// Serves the Message Body for the Popup.
+    /// </summary>
+    public string Message {
+        get {return _message;}
+        set {
+            OnPropertyChanged(_message);
+            OnPropertyChanged(Message);
+            _message = value;
+        }
+    }
+
     
-    public ErrorPopup() {
+    public ErrorPopup(string ErrorTitle, string ErrorMessage) {
+        // create the popup
         InitializeComponent();
+
+        // assign properties
+        Title = ErrorTitle;
+        Message = ErrorMessage;
+        PopupTitleLabel.Text = ErrorTitle;
+        PopupMessageLabel.Text = ErrorMessage;
     }
 }
