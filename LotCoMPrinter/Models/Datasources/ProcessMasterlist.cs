e616934b4deac41c49b6a2995863af9b0a4a890d
Mason Ritchason <masonritchason@gmail.com>
2025-03-18 09:27:17 -0400

    feat: implement new ProcessMasterlist data source
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Models/Datasources/ProcessMasterlist.cs
@@ -0,0 +1,41 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace LotCoMPrinter.Models.Datasources;
+
+/// <summary>
+/// Provides controlled access to the Process Masterlist data source.
+/// </summary>
+public static class ProcessMasterlist {
+    private const string _path = "\\\\144.133.122.1\\Lot Control Management\\Database\\part_control\\_process_masterlist";
+
+    /// <summary>
+    /// Asynchronously loads the data from the Process Masterlist data source.
+    /// </summary>
+    /// <returns>A JSON dictionary containing the Process Masterlist data.</returns>
+    /// <exception cref="JsonException"></exception>
+    private static async Task<JObject> LoadDataAsync() {
+        // read the masterlist file
+        JObject Masterlist = JObject.Parse(await File.ReadAllTextAsync(_path));
+        return Masterlist;
+    }
+
+    /// <summary>
+    /// Loads and queries the Process Masterlist data for data connected to ProcessFullName.
+    /// </summary>
+    /// <param name="ProcessFullName">The FULL name of a Process ("Code-Title") to query for.</param>
+    /// <returns>A JToken object containing the Process' data.</returns>
+    /// <exception cref="ArgumentException"></exception>
+    public static async Task<JToken> GetProcessData(string ProcessFullName) {
+        // load the data from the Masterlist
+        JObject FullData = await LoadDataAsync();
+        // attempt to access the data for the passed Process
+        JToken? SelectedData = FullData["Processes"]!.Where(x => x["FullName"]!.Equals(ProcessFullName)).First();
+        // check for a result and return
+        if (SelectedData.Equals(null)) {
+            throw new ArgumentException($"Could not retrieve data for process '{ProcessFullName}'.");
+        } else {
+            return SelectedData;
+        }
+    } 
+}
