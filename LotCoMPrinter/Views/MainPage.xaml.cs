6883636276f68ce293578fcb484767476aaaf85a
Mason Ritchason <masonritchason@gmail.com>
2025-03-20 15:36:48 -0400

    refactor(UI): pass additional UI controls to ViewModel.PrintRequest()
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Views/MainPage.xaml.cs
+++ LotCoMPrinter/Views/MainPage.xaml.cs
@@ -143,7 +143,7 @@ public partial class MainPage : ContentPage {
 		bool Printed;
 		// call the ViewModel's Print Request method
 		try {
-			Printed = await _viewModel.PrintRequest(PartPicker, QuantityEntry, JBKNumberEntry, LotNumberEntry, DeburrJBKNumberEntry, DieNumberEntry, ModelNumberEntry, ProductionDatePicker, ProductionShiftPicker, OperatorIDEntry);
+			Printed = await _viewModel.PrintRequest(ProcessPicker, PartPicker, QuantityEntry, JBKNumberEntry, LotNumberEntry, DeburrJBKNumberEntry, DieNumberEntry, ModelNumberEntry, BasketTypePicker, ProductionDatePicker, ProductionShiftPicker, OperatorIDEntry);
 		} catch (Exception _ex) {
 			// stop the Printing Indicator
 			_viewModel.Printing = false;
