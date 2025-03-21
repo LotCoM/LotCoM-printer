5f2b56953dee88680d3257c76445fa2db5f1bd43
Mason Ritchason <masonritchason@gmail.com>
2025-03-21 12:53:57 -0400

    refactor(LabelGenerator): integrate with new InterfaceCapture methods
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Labels/LabelGenerator.cs
+++ LotCoMPrinter/Models/Labels/LabelGenerator.cs
@@ -1,81 +1,10 @@
 using System.Drawing;
-using System.Reflection;
-using LotCoMPrinter.Models.Datasources;
 using LotCoMPrinter.Models.Exceptions;
 using LotCoMPrinter.Models.Validators;
 
 namespace LotCoMPrinter.Models.Labels;
 
 public static class LabelGenerator {
-    /// <summary>
-    /// Compiles the required data for the Process into a List of strings.
-    /// </summary>
-    /// <param name="Capture"></param>
-    /// <returns></returns>
-    private static async Task<List<string>> GetQRCodeData(InterfaceCapture Capture) {
-        List<string> QRCodeData = [];
-        await Task.Run(() => {
-            // retrieve the requirements for the SelectedProcess
-            List<string> Requirements = ProcessRequirements.GetProcessRequirements(Capture.SelectedProcess!.FullName);
-            string[] CodeExemptions = ["BasketType"];
-            // add each required field, in order, to a QR code data list (only if it is not exempt)
-            foreach (PropertyInfo _property in Capture.GetType().GetProperties()) {
-                // save the property's name
-                string _name = _property.Name;
-                if (Requirements.Contains(_name) && !CodeExemptions.Contains(_name)) {
-                    // retrieve the Process Name from the SelectedProcess
-                    if (_name.Equals("SelectedProcess")) {
-                        QRCodeData.Add(Capture.SelectedProcess!.FullName);
-                    // retrieve the Part Number from SelectedPart
-                    } else if (_name.Equals("SelectedPart")) {
-                        QRCodeData.Add(Capture.SelectedPart!.PartNumber);
-                        QRCodeData.Add(Capture.SelectedPart!.PartName);
-                    // retrieve the mm/dd/yyyy-hh:mm:ss string from ProductionDate
-                    } else if (_name.Equals("ProductionDate")) {
-                        QRCodeData.Add(new Timestamp(Capture.ProductionDate).Stamp);
-                    // just add the value of the property on the Capture object
-                    } else {
-                        QRCodeData.Add((string)_property.GetValue(Capture)!);
-                    }
-                }
-            }
-        });
-        return QRCodeData;
-    }
-
-    /// <summary>
-    /// Formats an InterfaceCapture's data into a Label Field text body (list of strings).
-    /// </summary>
-    /// <param name="Capture"></param>
-    /// <returns></returns>
-    private static async Task<List<string>> GetPrintedLabelFields(InterfaceCapture Capture) {
-        List<string> LabelFields = [];
-        await Task.Run(() => {
-            // retrieve the requirements for the SelectedProcess
-            List<string> Requirements = ProcessRequirements.GetProcessRequirements(Capture.SelectedProcess!.FullName);
-            string[] PrintExemptions = ["SelectedProcess", "BasketType", "OperatorID"];
-            // add each required field based on the need to print
-            foreach (PropertyInfo _property in Capture.GetType().GetProperties()) {
-                // save the property's name
-                string _name = _property.Name;
-                // only add the property to the list if it is required and not exempt from printing
-                if (Requirements.Contains(_name) && !PrintExemptions.Contains(_name)) {
-                    // retrieve the Part Number from SelectedPart
-                    if (_name.Equals("SelectedPart")) {
-                        LabelFields.Add(Capture.SelectedPart!.PartNumber);
-                    // retrieve the mm/dd/yyyy-hh:mm:ss string from ProductionDate
-                    } else if (_name.Equals("ProductionDate")) {
-                        LabelFields.Add(new Timestamp(Capture.ProductionDate).Stamp);
-                    // just add the value of the property on the Capture object
-                    } else {
-                        LabelFields.Add((string)_property.GetValue(Capture)!);
-                    }
-                }
-            }
-        });
-        return LabelFields;
-    }
-
     /// <summary>
     /// Generates a Full Label image object that mirrors the appearance of the physical Full Basket Label.
     /// Composition of Asynchronous tasks.
@@ -94,13 +23,13 @@ public static class LabelGenerator {
             throw new LabelBuildException($"Failed to construct new Label due to the following exception:\n{_ex.Message}");
         }
         // create a List of Label Fields from the Capture and generate a QR Code from that List
-        List<string> QRCodeData = await GetQRCodeData(Capture);
+        List<string> QRCodeData = Capture.FormatAsQRCodeData();
         QRCode? LabelCode = new QRCode(QRCodeData);
         // apply the header, the QR Code, and the Label Data to the Label
         await NewLabel.AddHeaderAsync(LabelHeader);
         await NewLabel.AddPartNameAsync(Capture.SelectedPart!.PartName);
         await NewLabel.AddQRCodeAsync(LabelCode);
-        await NewLabel.AddLabelFieldsAsync(await GetPrintedLabelFields(Capture));
+        await NewLabel.AddLabelFieldsAsync(Capture.FormatAsLabelBodyText());
         // return the Label image
         return NewLabel.GetImage();
     }
@@ -123,14 +52,13 @@ public static class LabelGenerator {
             throw new LabelBuildException($"Failed to construct new Label due to the following exception:\n{_ex.Message}");
         }
         // create a List of Label Fields from the Capture and generate a QR Code from that List
-        List<string> QRCodeData = ["PARTIAL"];
-        QRCodeData.AddRange(await GetQRCodeData(Capture));
+        List<string> QRCodeData = Capture.FormatAsQRCodeData();
         QRCode? LabelCode = new QRCode(QRCodeData);
         // apply the header, the QR Code, and the Label Data to the Label
         await NewLabel.AddHeaderAsync(LabelHeader);
         await NewLabel.AddPartNameAsync(Capture.SelectedPart!.PartName);
         await NewLabel.AddQRCodeAsync(LabelCode);
-        await NewLabel.AddLabelFieldsAsync(await GetPrintedLabelFields(Capture));
+        await NewLabel.AddLabelFieldsAsync(Capture.FormatAsLabelBodyText());
         // return the Label image
         return NewLabel.GetImage();
     }
