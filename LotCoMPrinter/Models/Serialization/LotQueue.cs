196ca70b393b38dd7780a32a45d807cd4c0c6591
Mason Ritchason <masonritchason@gmail.com>
2025-03-25 10:12:24 -0400

    refactor(SerialQueues): change serialization level from Model to Part Numbers
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Serialization/LotQueue.cs
+++ LotCoMPrinter/Models/Serialization/LotQueue.cs
@@ -38,41 +38,41 @@ public static class LotQueue {
     }
 
     /// <summary>
-    /// Retrieves the currently queued Lot number for the Model number WITHOUT incrementing the Queue.
+    /// Retrieves the currently queued Lot number for the Part Number WITHOUT incrementing the Queue.
     /// </summary>
-    /// <param name="ModelNumber">The Model number to access the queue of.</param>
+    /// <param name="PartNumber">The Part Number to access the queue of.</param>
     /// <returns></returns>
     /// <exception cref="ArgumentException"></exception>
-    public static async Task<int> QueuedAsync(string ModelNumber) {
+    public static async Task<int> QueuedAsync(string PartNumber) {
         // retrieve the Queue Dictionary
         Dictionary<string, int> QueueDictionary = await DeserializeAsync();
-        // access the Lot number for the Model number key
-        int QueuedLot;
+        // access the Lot number for the Part Number key
+        int QueuedJBK;
         try {
-            QueuedLot = QueueDictionary[ModelNumber];
+            QueuedJBK = QueueDictionary[PartNumber];
         } catch {
-            throw new ArgumentException($"Could not find a Lot # queue for the model: {ModelNumber}.");
+            throw new ArgumentException($"Could not find a Lot # queue for the Part: {PartNumber}.");
         }
-        return QueuedLot;
+        return QueuedJBK;
     }
 
     /// <summary>
-    /// Retrieves the currently queued Lot number for the Model number, increments that Queue, and overwrites the Queue file.
+    /// Retrieves the currently queued Lot number for the Part Number, increments that Queue, and overwrites the Queue file.
     /// This method WILL consume a Lot number from the queue when called.
     /// </summary>
-    /// <param name="ModelNumber"></param>
-    public static async Task<string> ConsumeAsync(string ModelNumber) {
+    /// <param name="PartNumber"></param>
+    public static async Task<string> ConsumeAsync(string PartNumber) {
         // retrieve the Queue Dictionary
         Dictionary<string, int> QueueDictionary = await DeserializeAsync();
         int Consumed = await Task.Run(() => {
-            // access the queued Lot number for the Model number
-            int Unincremented = QueueDictionary[ModelNumber];
+            // access the queued Lot number for the Part Number
+            int Unincremented = QueueDictionary[PartNumber];
             // if the queued number is 999999999, reset to 1
             if (Unincremented >= 999999999) {
                 Unincremented = 0;
             }
             // increment the Queued Lot number and save the new Queue version
-            QueueDictionary[ModelNumber] = Unincremented + 1;
+            QueueDictionary[PartNumber] = Unincremented + 1;
             return Unincremented;
         });
         // save the incremented queue
