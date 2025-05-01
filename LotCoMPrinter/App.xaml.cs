f8c9074bb4e780b5b257637cab715cb52ed1c058
Mason Ritchason <masonritchason@gmail.com>
2025-05-01 14:52:01 -0400

    refactor(App): implement Window TitleBar and NavigationPage navigation mode
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/App.xaml.cs
+++ LotCoMPrinter/App.xaml.cs
@@ -1,12 +1,30 @@
-﻿namespace LotCoMPrinter;
+﻿using LotCoMPrinter.Views;
 
-public partial class App : Application {
+namespace LotCoMPrinter;
 
-	public App() {
+public partial class App : Application 
+{
+
+	public App() 
+	{
 		InitializeComponent();
 	}
 
-	protected override Window CreateWindow(IActivationState? activationState) {
-		return new Window(new AppShell());
+	protected override Window CreateWindow(IActivationState? activationState) 
+	{
+		// create the Main Window
+		Window MainWindow = new Window(new NavigationPage(new MainPage()));
+		// get the version of the assembly at entry (the app version number)
+		string Version = System.Reflection.Assembly.GetEntryAssembly()!.GetName().Version!.ToString();
+		// create a TitleBar for the Window
+		TitleBar MainWindowTitleBar = new TitleBar 
+		{
+			Icon = "lotcom_logo.png",
+			Title = "LotCom WIP Labels",
+			Subtitle = $"v{Version}"      
+		};
+		// add the TitleBar to the Main Window
+		MainWindow.TitleBar = MainWindowTitleBar;
+		return MainWindow;
 	}
 }
