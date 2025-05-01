7965139ab205867f95a90a0165a8358c93816001
Mason Ritchason <masonritchason@gmail.com>
2025-05-01 13:51:09 -0400

    feat(PartialDataSet): implement PartialDataSet to encapsulate a set of Partial production data
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Models/Options/PartialDataSet.cs
@@ -0,0 +1,18 @@
+using CommunityToolkit.Mvvm.ComponentModel;
+using LotCoMPrinter.Models.Datasources;
+
+namespace LotCoMPrinter.Models.Options;
+
+/// <summary>
+/// Provides a structure to capture Data related to a Partially-completed Basket.
+/// </summary>
+public partial class PartialDataSet(int Quantity, VariableFieldSet VariableFields, int Shift, string Operator) : ObservableObject()
+{
+    private int Quantity = Quantity;
+
+    private VariableFieldSet VariableFields = VariableFields;
+
+    private int Shift = Shift;
+
+    private string Operator = Operator;
+}
