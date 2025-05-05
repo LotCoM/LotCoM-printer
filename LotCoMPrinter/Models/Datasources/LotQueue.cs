b13a66257e4e7036d80c9f56760c15f38bc3bfd1
Mason Ritchason <masonritchason@gmail.com>
2025-05-05 13:08:32 -0400

    refactor(SerialQueus): integrate with SerializationModes
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/LotQueue.cs
+++ LotCoMPrinter/Models/Datasources/LotQueue.cs
@@ -2,6 +2,6 @@ namespace LotCoMPrinter.Models.Datasources;
 
 public partial class LotQueue() : 
     SerialQueue(QueuePath: "\\\\144.133.122.1\\Lot Control Management\\Database\\process_control\\serial_queues\\_lot_queue.json", 
-                Serialization: "Lot", 
+                Mode: SerializationModes.Lot, 
                 Limit: 999999999) {
 }
--- LotCoMPrinter/Models/Datasources/SerialQueue.cs
+++ LotCoMPrinter/Models/Datasources/SerialQueue.cs
@@ -2,27 +2,43 @@ using Newtonsoft.Json;
 
 namespace LotCoMPrinter.Models.Datasources;
 
-public class SerialQueue(string QueuePath, string Serialization, int Limit) {
-    
-    private readonly string _queuePath = QueuePath;
-    private readonly string _serialization = Serialization;
-    private readonly int _limit = Limit;
+public class SerialQueue(string QueuePath, SerializationModes Mode, int Limit) 
+{
+    /// <summary>
+    /// The path to the file containing queues for Mode.
+    /// </summary>
+    private readonly string QueuePath = QueuePath;
+
+    /// <summary>
+    /// The Serialization method for this queue.
+    /// </summary>
+    private readonly SerializationModes Mode = Mode;
+
+    /// <summary>
+    /// The last number allowed for use by this queue.
+    /// </summary>
+    private readonly int Limit = Limit;
 
     /// <summary>
     /// Reads the Serial Queue file and deserializes it into a Queue Dictionary.
     /// </summary>
     /// <returns></returns>
     /// <exception cref="JsonException"></exception>
-    private async Task<Dictionary<string, int>> DeserializeAsync() {
+    private async Task<Dictionary<string, int>> DeserializeAsync() 
+    {
         // read the Serial Queue file
-        string QueueFile = await File.ReadAllTextAsync(_queuePath);
-        Dictionary<string, int> QueueDictionary = await Task.Run(() => {
+        string QueueFile = await File.ReadAllTextAsync(QueuePath);
+        Dictionary<string, int> QueueDictionary = await Task.Run(() => 
+        {
             // attempt to deserialize the Serial Queue file text into a dictionary
-            try {
+            try 
+            {
                 Dictionary<string, int> Dict = JsonConvert.DeserializeObject<Dictionary<string, int>>(QueueFile)!;
                 return Dict;
-            } catch {
-                throw new JsonException($"Failed to deserialize the {_serialization} # Queue.");
+            } 
+            catch 
+            {
+                throw new JsonException($"Failed to deserialize the {Mode} # Queue.");
             }
         });
         return QueueDictionary;
@@ -32,11 +48,12 @@ public class SerialQueue(string QueuePath, string Serialization, int Limit) {
     /// Overwrites the Serial Queue file with a new version of the Queue.
     /// </summary>
     /// <param name="QueueDictionary">The modified queue dictionary.</param>
-    private async Task SaveAsync(Dictionary<string, int> QueueDictionary) {
+    private async Task SaveAsync(Dictionary<string, int> QueueDictionary) 
+    {
         // serialize the QueueDictionary to a JSON string
         string Serialized = JsonConvert.SerializeObject(QueueDictionary);
         // write the serialized string to the Serial Queue file
-        await File.WriteAllTextAsync(_queuePath, Serialized);
+        await File.WriteAllTextAsync(QueuePath, Serialized);
     }
 
     /// <summary>
@@ -45,15 +62,19 @@ public class SerialQueue(string QueuePath, string Serialization, int Limit) {
     /// <param name="PartNumber">The Part Number to access the Queue of.</param>
     /// <returns></returns>
     /// <exception cref="ArgumentException"></exception>
-    public async Task<int> QueuedAsync(string PartNumber) {
+    public async Task<int> QueuedAsync(string PartNumber) 
+    {
         // retrieve the Queue Dictionary
         Dictionary<string, int> QueueDictionary = await DeserializeAsync();
         // access the Serial Number for the Part Number key
         int QueuedSerial;
-        try {
+        try 
+        {
             QueuedSerial = QueueDictionary[PartNumber];
-        } catch {
-            throw new ArgumentException($"Could not find a {_serialization} # Queue for the Part: {PartNumber}.");
+        } 
+        catch 
+        {
+            throw new ArgumentException($"Could not find a {Mode} # Queue for the Part: {PartNumber}.");
         }
         return QueuedSerial;
     }
@@ -63,14 +84,17 @@ public class SerialQueue(string QueuePath, string Serialization, int Limit) {
     /// This method WILL consume a Serial Number from the Queue when called.
     /// </summary>
     /// <param name="PartNumber"></param>
-    public async Task<string> ConsumeAsync(string PartNumber) {
+    public async Task<string> ConsumeAsync(string PartNumber) 
+    {
         // retrieve the Queue Dictionary
         Dictionary<string, int> QueueDictionary = await DeserializeAsync();
-        int Consumed = await Task.Run(() => {
+        int Consumed = await Task.Run(() => 
+        {
             // access the queued Serial Number for the Part Number
             int Unincremented = QueueDictionary[PartNumber];
             // if the queued Serial Number is at the limit, reset to 1
-            if (Unincremented >= _limit) {
+            if (Unincremented >= Limit) 
+            {
                 Unincremented = 0;
             }
             // increment the queued Serial Number and save the new Queue version
