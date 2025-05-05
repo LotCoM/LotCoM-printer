3365a82f8885e79c8646dc97422a3de017a354b2
Mason Ritchason <masonritchason@gmail.com>
2025-05-05 13:49:33 -0400

    refactor(PartialDataSet): apply ObservableProperty attribute to all properties
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Options/PartialDataSet.cs
+++ LotCoMPrinter/Models/Options/PartialDataSet.cs
@@ -8,20 +8,23 @@ namespace LotCoMPrinter.Models.Options;
 /// <param name="Quantity">The number of Parts produced during the Shift captured by the PartialDataSet.</param>
 /// <param name="Shift">The Shift Number captured by the PartialDataSet.</param>
 /// <param name="Operator">The Operator who created the PartialDataSet.</param>
-public partial class PartialDataSet(int Quantity, int Shift, string Operator) : ObservableObject()
+public partial class PartialDataSet(int? Quantity = null, int? Shift = null, string? Operator = null) : ObservableObject()
 {
+    [ObservableProperty]
     /// <summary>
     /// The number of Parts produced during the Shift captured by the PartialDataSet.
     /// </summary>
-    public int Quantity = Quantity;
+    public partial int? Quantity {get; set;} = Quantity;
 
+    [ObservableProperty]
     /// <summary>
     /// The Shift Number captured by the PartialDataSet.
     /// </summary>
-    public int Shift = Shift;
+    public partial int? Shift {get; set;} = Shift;
 
+    [ObservableProperty]
     /// <summary>
     /// The Operator who created the PartialDataSet.
     /// </summary>
-    public string Operator = Operator;
+    public partial string? Operator {get; set;} = Operator;
 }
