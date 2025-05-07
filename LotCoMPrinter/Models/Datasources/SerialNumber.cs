df61dd14364ef0b2d93107506d523554c5472c3e
Mason Ritchason <masonritchason@gmail.com>
2025-05-07 15:21:34 -0400

    feat(SerialNumber): implement SerialNumber class
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Models/Datasources/SerialNumber.cs
@@ -0,0 +1,96 @@
+namespace LotCoMPrinter.Models.Datasources;
+
+public class SerialNumber
+{
+    /// <summary>
+    /// The mode of Serialization that the Serial Number uses.
+    /// </summary>
+    public SerializationModes Mode {get;} = SerializationModes.None;
+
+    /// <summary>
+    /// The Serial Number's literal value.
+    /// </summary>
+    public object? LiteralValue {get; private set;} = null;
+
+    /// <summary>
+    /// The Type of the Serial Number's Value property.
+    /// </summary>
+    public Type LiteralType {get; private set;} = typeof(object);
+
+    /// <summary>
+    /// Confirms that the passed Literal Value can be used to create a Serial Number of the passed Serialization Mode.
+    /// </summary>
+    /// <returns>true (if successful).</returns>
+    /// <exception cref="NullReferenceException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    private bool ValidateLiteral()
+    {
+        if (LiteralValue is null)
+        {
+            throw new NullReferenceException("Cannot create a Serial Number without a value.");
+        }
+        if (Mode == SerializationModes.None)
+        {
+            throw new NullReferenceException("Cannot create a Serial Number without a Serialization Mode.");
+        }
+        if (Mode == SerializationModes.JBK)
+        {
+            // set the LiteralType property to int and confirm the Value property is valid
+            if (!LiteralType.GetType().Equals(typeof(int)))
+            {
+                LiteralType = typeof(int);
+            }
+            if (!LiteralValue.GetType().Equals(LiteralType.GetType()))
+            {
+                try
+                {
+                    LiteralValue = int.Parse(LiteralValue.ToString()!);
+                }
+                catch
+                {
+                    throw new ArgumentException($"Cannot parse a valid 'int' from '{LiteralValue}' to apply to the Serial Number with SerializationMode 'JBK'.");
+                }
+            }
+        }
+        else
+        {
+            // set the LiteralType property to string and confirm the Value property is valid
+            if (!LiteralType.GetType().Equals(typeof(string)))
+            {
+                LiteralType = typeof(string);
+            }
+            if (!LiteralValue.GetType().Equals(LiteralType.GetType()))
+            {
+                try
+                {
+                    LiteralValue = LiteralValue.ToString();
+                }
+                catch
+                {
+                    throw new ArgumentException($"Cannot parse a valid 'string' from '{LiteralValue}' to apply to the Serial Number with SerializationMode 'Lot'.");
+                }
+            }
+        }
+        // the Serial Number can be created with the passed Literal Value
+        return true;
+    }
+
+    /// <summary>
+    /// Create a new Serial Number using SerializationMode. 
+    /// Verifies that LiteralValue can be used as a Value for a Serial Number using the passed Serialization Mode.
+    /// </summary>
+    /// <param name="Mode">The mode of Serialization this Serial Number uses.</param>
+    /// <param name="LiteralValue">The Value to attempt to apply to this Serial Number.</param>
+    /// <exception cref="ArgumentException"></exception>
+    public SerialNumber(SerializationModes Mode, object LiteralValue)
+    {
+        this.Mode = Mode;
+        this.LiteralValue = LiteralValue;
+        LiteralType = LiteralValue.GetType();
+        // ensure that the passed literal value matches the required Serial Mode type
+        if (!ValidateLiteral())
+        {
+            throw new ArgumentException($"Cannot create a Serial Number of this SerializationMode with the passed literal value '{LiteralValue}'");
+        }
+    }
+}
