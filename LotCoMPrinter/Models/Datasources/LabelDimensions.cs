698342c3b3fa6c6ffc9701f6aaf0e5af1ab5da30
Mason Ritchason <masonritchason@gmail.com>
2025-05-07 11:33:32 -0400

    refactor(LabelDimensions): remove enums to allow DPI based resolution scaling
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/LabelDimensions.cs
+++ LotCoMPrinter/Models/Datasources/LabelDimensions.cs
@@ -1,3 +1,5 @@
+using LotCoMPrinter.Models.Services;
+
 namespace LotCoMPrinter.Models.Datasources;
 
 /// <summary>
@@ -8,75 +10,75 @@ public class LabelDimensions
     /// <summary>
     /// Provides default Label sizes (length of each side as a square Label).
     /// </summary>
-    public enum LabelSizes
+    public static class LabelSizes
     {
         /// <summary>
         /// 3.00 x 3.00 inch Label size.
         /// </summary>
-        Default = 1440
+        public const int Default = 1440;
     }
 
     /// <summary>
     /// Provides default QR Code sizes (length of each side as a square QR Code).
     /// </summary>
-    public enum CodeSizes
+    public static class CodeSizes
     {
         /// <summary>
         /// Roughly 1.20 x 1.20 inch QR Code 
         /// (smallest Code size while preserving peak readability).
         /// </summary>
-        Default = 600
+        public const int Default = 600;
     }
 
     /// <summary>
     /// Provides default text sizes.
     /// </summary>
-    public enum TextSizes
+    public static class TextSizes
     {
-        Tiny = 42,
-        Small = 76,
-        Medium = 128,
-        Large = 192
+        public const int Tiny = 42;
+        public const int Small = 76;
+        public const int Medium = 128;
+        public const int Large = 192;
     }
 
     /// <summary>
     /// Provides default padding (space between items) levels.
     /// </summary>
-    public enum PaddingLevels
+    public static class PaddingLevels
     {
-        Thin = 12,
-        Wide = 24,
+        public const int Thin = 12;
+        public const int Wide = 24;
     }
 
     /// <summary>
     /// The length of each side of the Label (square).
     /// </summary>
-    public LabelSizes Dimension = LabelSizes.Default;
+    public int Dimension = LabelSizes.Default;
 
     /// <summary>
     /// The length of each side of the QR Code on the Label (square).
     /// </summary>
-    public CodeSizes CodeDimension = CodeSizes.Default;
+    public int CodeDimension = CodeSizes.Default;
 
     /// <summary>
     /// The font size for small text on the Label.
     /// </summary>
-    public TextSizes TextSizeSmall = TextSizes.Tiny;
+    public int TextSizeSmall = TextSizes.Tiny;
 
     /// <summary>
     /// The font size for medium text on the Label.
     /// </summary>
-    public TextSizes TextSizeMedium = TextSizes.Small;
+    public int TextSizeMedium = TextSizes.Small;
     
     /// <summary>
     /// The font size for large text on the Label.
     /// </summary>
-    public TextSizes TextSizeLarge = TextSizes.Large;
+    public int TextSizeLarge = TextSizes.Large;
 
     /// <summary>
     /// Padding width/height applied between each item on the Label.
     /// </summary>
-    public PaddingLevels InternalPadding = PaddingLevels.Thin;
+    public int InternalPadding = PaddingLevels.Thin;
 
     /// <summary>
     /// The horizontal position of the Label's Heading text.
@@ -119,6 +121,31 @@ public class LabelDimensions
     public int BodyVerticalAnchor;
 
     /// <summary>
+    /// Converts dimension values to match the DPI scaling of the Device.
+    /// Multiplies the default values by the set DPI scale.
+    /// </summary>
+    private void AdjustScale()
+    {
+        // retreive the DPI scale for the device's display
+        double DpiScale = DpiUtilities.GetDeviceDpiScale();
+        // scale the Label's dimensions to the DPI
+        Dimension = Convert.ToInt32(Dimension * DpiScale);
+        CodeDimension = Convert.ToInt32(CodeDimension * DpiScale);
+        TextSizeSmall = Convert.ToInt32(TextSizeSmall * DpiScale);
+        TextSizeMedium = Convert.ToInt32(TextSizeMedium * DpiScale);
+        TextSizeLarge = Convert.ToInt32(TextSizeLarge * DpiScale);
+        InternalPadding = Convert.ToInt32(InternalPadding * DpiScale);
+        HeadingHorizontalAnchor = Convert.ToInt32(HeadingHorizontalAnchor * DpiScale);
+        HeadingVerticalAnchor = Convert.ToInt32(HeadingVerticalAnchor * DpiScale);
+        PartNameHorizontalAnchor = Convert.ToInt32(PartNameHorizontalAnchor * DpiScale);
+        PartNameVerticalAnchor = Convert.ToInt32(PartNameVerticalAnchor * DpiScale);
+        CodeHorizontalAnchor = Convert.ToInt32(CodeHorizontalAnchor * DpiScale);
+        CodeVerticalAnchor = Convert.ToInt32(CodeVerticalAnchor * DpiScale);
+        BodyHorizontalAnchor = Convert.ToInt32(BodyHorizontalAnchor * DpiScale);
+        BodyVerticalAnchor = Convert.ToInt32(BodyVerticalAnchor * DpiScale);
+    }
+
+    /// <summary>
     /// Creates a Dimensions control structure for a Label object.
     /// </summary>
     /// <param name="Dimension">The length of each side of the Label (square).</param>
@@ -135,31 +162,31 @@ public class LabelDimensions
     /// <param name="CodeVerticalAnchor">The vertical (top) position of the QR Code on the Label.</param>
     /// <param name="BodyHorizontalAnchor">The horizontal position of the Label's Data Fields text.</param>
     /// <param name="BodyVerticalAnchor">The vertical position of the Label's Data Fields text.</param>
-    public LabelDimensions(LabelSizes? Dimension, CodeSizes? CodeDimension, TextSizes? TextSizeSmall, TextSizes? TextSizeMedium, TextSizes? TextSizeLarge, PaddingLevels? InternalPadding, int HeadingHorizontalAnchor, int HeadingVerticalAnchor, int PartNameHorizontalAnchor, int PartNameVerticalAnchor, int CodeHorizontalAnchor, int CodeVerticalAnchor, int BodyHorizontalAnchor, int BodyVerticalAnchor)
+    public LabelDimensions(int? Dimension, int? CodeDimension, int? TextSizeSmall, int? TextSizeMedium, int? TextSizeLarge, int? InternalPadding, int HeadingHorizontalAnchor, int HeadingVerticalAnchor, int PartNameHorizontalAnchor, int PartNameVerticalAnchor, int CodeHorizontalAnchor, int CodeVerticalAnchor, int BodyHorizontalAnchor, int BodyVerticalAnchor)
     {
         if (Dimension is not null)
         {
-            this.Dimension = (LabelSizes)Dimension;
+            this.Dimension = (int)Dimension;
         }
         if (CodeDimension is not null)
         {
-            this.CodeDimension = (CodeSizes)CodeDimension;
+            this.CodeDimension = (int)CodeDimension;
         }
         if (TextSizeSmall is not null)
         {
-            this.TextSizeSmall = (TextSizes)TextSizeSmall;
+            this.TextSizeSmall = (int)TextSizeSmall;
         }
         if (TextSizeMedium is not null)
         {
-            this.TextSizeMedium = (TextSizes)TextSizeMedium;
+            this.TextSizeMedium = (int)TextSizeMedium;
         }
         if (TextSizeLarge is not null)
         {
-            this.TextSizeLarge = (TextSizes)TextSizeLarge;
+            this.TextSizeLarge = (int)TextSizeLarge;
         }
         if (InternalPadding is not null)
         {
-            this.InternalPadding = (PaddingLevels)InternalPadding;
+            this.InternalPadding = (int)InternalPadding;
         }
         this.HeadingHorizontalAnchor = HeadingHorizontalAnchor;
         this.HeadingVerticalAnchor = HeadingVerticalAnchor;
@@ -169,5 +196,7 @@ public class LabelDimensions
         this.CodeVerticalAnchor = CodeVerticalAnchor;
         this.BodyHorizontalAnchor = BodyHorizontalAnchor;
         this.BodyVerticalAnchor = BodyVerticalAnchor;
+        // adjust the DPI Scaling of the Label design
+        AdjustScale();
     }
 }
