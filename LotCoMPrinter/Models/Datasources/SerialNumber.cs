b023000c21f2ac5b10579a41a4077b6bd875515c
Mason Ritchason <masonritchason@gmail.com>
2025-05-08 08:44:52 -0400

    feat(SerialNumber): implement ToJSON and ParseJSON methods
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/SerialNumber.cs
+++ LotCoMPrinter/Models/Datasources/SerialNumber.cs
@@ -1,3 +1,6 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
 namespace LotCoMPrinter.Models.Datasources;
 
 public class SerialNumber
@@ -98,5 +101,131 @@ public class SerialNumber
         {
             throw new ArgumentException($"Cannot create a Serial Number of this SerializationMode with the passed literal value '{LiteralValue}'");
         }
+    }
+
+    /// <summary>
+    /// Formats the Serial Number as a JSON string that can be written to a File and parsed as JSON text.
+    /// </summary>
+    /// <returns></returns>
+    public string ToJSON()
+    {   
+        return 
+            "{" +
+                $"Mode:{Mode}," +
+                "Part:{" +
+                    $"PartNumber:{Part!.PartNumber}," +
+                    $"Process{Part!.ParentProcess}" +
+                "}," +
+                $"LiteralValue:{LiteralValue}," +
+                $"LiteralType:{LiteralType}" +
+            "}";
+    }
+
+    /// <summary>
+    /// Attempts to parse a SerialNumber object from a JSON formatted string.
+    /// </summary>
+    /// <remarks>
+    /// Throws JsonException if there were any errors parsing any part of the Serial Number from Line.
+    /// Throws ArgumentException if the parsed Part Number was not defined for the parsed Process.
+    /// </remarks>
+    /// <param name="Line"></param>
+    /// <returns>A SerialNumber object.</returns>
+    /// <exception cref="JsonException"></exception>
+    /// <exception cref="ArgumentException"></exception>
+    public static async Task<SerialNumber> ParseJSON(string Line)
+    {
+        SerializationModes Mode;
+        Part Part;
+        object LiteralValue;
+        Type LiteralType;
+        // parse Line into JSON
+        JObject JSON = JObject.Parse(Line);
+        JToken? RawMode = JSON["Mode"];
+        JToken? RawPart = JSON["Part"];
+        JToken? RawLiteralValue = JSON["LiteralValue"];
+        JToken? RawLiteralType = JSON["LiteralType"];
+        // confirm the Mode key has a valid value and convert it to a SerializationMode
+        if (RawMode is null)
+        {
+            throw new JsonException($"No Serialization Mode found in cached Serial Number '{Line}'.");
+        }
+        if (RawMode.ToString().Equals("JBK"))
+        {
+            Mode = SerializationModes.JBK;
+        } else if (RawMode.ToString().Equals("Lot"))
+        {
+            Mode = SerializationModes.Lot;
+        } else {
+            throw new JsonException($"Invalid Serialization Mode '{RawMode}'");
+        }
+        // confirm the Part key has a valid value and convert it to a Part
+        if (RawPart is null)
+        {
+            throw new JsonException($"No Part found in cached Serial Number '{Line}'.");
+        }
+        string? PartNumber;
+        string? ProcessName;
+        try
+        {
+            PartNumber = RawPart["PartNumber"]!.ToString();
+            ProcessName = RawPart["Process"]!.ToString();
+        }
+        catch
+        {
+            throw new JsonException($"Could not parse a Part Number and/or Process from cached Serial Number '{Line}'.");
+        }
+        if (PartNumber is null || ProcessName is null)
+        {
+            throw new JsonException($"Could not parse a Part Number and/or Process from cached Serial Number '{Line}'.");
+        }
+        try
+        {
+            Part = await new ProcessData().GetProcessPartDataAsync(PartNumber, ProcessName);
+        }
+        catch
+        {
+            throw new ArgumentException($"The Part '{PartNumber}' for Process '{ProcessName}' was not defined.");
+        }
+        // confirm the LiteralType key has a non-null value and is either int or string
+        if (RawLiteralType is null) 
+        {
+            throw new JsonException($"No LiteralType found in cached Serial Number '{Line}'.");
+        }
+        Type? ParsedType = Type.GetType(RawLiteralType.ToString());
+        if (ParsedType is not null && 
+            (ParsedType.Equals(typeof(int)) || ParsedType.Equals(typeof(string))))
+        {
+            LiteralType = ParsedType;
+        }
+        else
+        {
+            throw new JsonException($"Invalid LiteralType '{ParsedType}'.");
+        }
+        // confirm the LiteralValue key has a valid value and that it matches the LiteralType
+        if (RawLiteralValue is null) 
+        {
+            throw new JsonException($"No LiteralValue found in cached Serial Number '{Line}'.");
+        }
+        if (Mode == SerializationModes.JBK)
+        {
+            try
+            {
+                LiteralValue = int.Parse(RawLiteralValue.ToString());
+            }
+            catch
+            {
+                throw new JsonException($"Invalid LiteralValue '{RawLiteralValue}' for SerializationMode 'JBK'.");
+            }
+        }
+        else
+        {
+            LiteralValue = int.Parse(RawLiteralValue.ToString());
+        }
+        if (!LiteralValue.GetType().Equals(LiteralType))
+        {
+            throw new JsonException($"LiteralValue '{LiteralValue}' is of an invalid Type for SerializationMode 'JBK'.");
+        }
+        // use JToken approach from Client app to parse out data fields
+        return new SerialNumber(Mode, Part, LiteralValue);
     }
 }
