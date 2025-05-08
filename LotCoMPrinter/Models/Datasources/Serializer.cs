a42ef3f5de1a1fae79a3a2108fd8a31c7fbab054
Mason Ritchason <masonritchason@gmail.com>
2025-05-08 10:30:14 -0400

    refactor(Serializer): implement SerialNumber class; major optimizations
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/Serializer.cs
+++ LotCoMPrinter/Models/Datasources/Serializer.cs
@@ -3,84 +3,52 @@ namespace LotCoMPrinter.Models.Datasources;
 public static class Serializer 
 {
     /// <summary>
-    /// Checks the Serial Cache for a Cached number under this Part Number and retrieves a new one if no Cache exists.
+    /// Checks the Serial Cache for a SerialNumber for Part.
     /// </summary>
-    /// <param name="PartNumber"></param>
-    /// <param name="Mode"></param>
-    /// <returns></returns>
-    private static async Task<string> GetSerialNumber(string PartNumber, SerializationModes Mode) 
+    /// <param name="Part"></param>
+    /// <returns>A SerialNumber object (if one was cached for Part).</returns>
+    private static async Task<SerialNumber?> CheckCache(Part Part)
     {
-        // run the serial number retrieval on a new CPU thread
-        string SerialNumber = await Task.Run(async () => 
-        {
-            // create a new SerialCacheController to interact with the Serial Cache Files
-            SerialCacheController SerialCache = new SerialCacheController();
-            // check if there is already a cached number
-            string? Number = await SerialCache.FindNumberForPart(PartNumber);
-            // if there was no serial number found, consume and cache a new one
-            if (Number is null) {
-                if (Mode == SerializationModes.JBK) 
-                {
-                    // consume the queued JBK number for this Model
-                    Number = await new JBKQueue().ConsumeAsync(PartNumber);
-                } 
-                else 
-                {
-                    // consume the queued Lot number for this Model and cache it under the part number
-                    Number = await new LotQueue().ConsumeAsync(PartNumber);
-                }
-            }
-            return Number;
-        });
-        return SerialNumber;
+        return await SerialCacheController.FindNumberForPart(Part);
+        
     }
 
     /// <summary>
-    /// Formats SerialNumber according to the Serialization Mode format requirements.
+    /// Retrieves a SerialNumber for Part.
     /// </summary>
-    /// <param name="SerialNumber"></param>
+    /// <param name="Part"></param>
     /// <param name="Mode"></param>
     /// <returns></returns>
-    private static string FormatSerialNumber(string SerialNumber, SerializationModes Mode) {
-        // enforce leading zero-padding format
+    private static async Task<SerialNumber> GetSerialNumber(Part Part, SerializationModes Mode) 
+    {
+        // check for a cached SerialNumber for Part
+        SerialNumber? Number = await CheckCache(Part);
+        if (Number is not null)
+        {
+            return Number;
+        }
+        // no SerialNumber was cached for Part; retrieve a new one
         if (Mode == SerializationModes.JBK) 
         {
-            // enforce 3-length format
-            while (SerialNumber.Length < 3) 
-            {
-                SerialNumber = "0" + SerialNumber;
-            }
+            Number = await new JBKQueue().ConsumeAsync(Part);
         } 
         else 
         {
-            // enforce 9-length format
-            while (SerialNumber.Length < 9) 
-            {
-                SerialNumber = "0" + SerialNumber;
-            }
+            Number = await new LotQueue().ConsumeAsync(Part);
         }
-        return SerialNumber;
+        return Number;
     }
 
     /// <summary>
     /// Assigns a Serial Number to use for a new Label.
     /// </summary>
     /// <param name="Capture">An InterfaceCapture object to use as a source for serialization information.</param>
-    /// <returns>A Serial Number string.</returns>
-    public static async Task<string?> Serialize(InterfaceCapture Capture) 
+    /// <returns>A SerialNumber object.</returns>
+    public static async Task<SerialNumber?> Serialize(InterfaceCapture Capture) 
     {
-        // retrieve values from the Capture to improve processing time
-        SerializationModes Mode = Capture.Process.Serialization;
-        Part SelectedPart = Capture.Part;
-        string PartNumber = SelectedPart.PartNumber;
-        // get the serial number for this label
-        string SerialNumber = await GetSerialNumber(PartNumber, Mode);
-        // format the Serial Number
-        SerialNumber = FormatSerialNumber(SerialNumber, Mode);
-        // cache the Serial Number under the part number
-        SerialCacheController SerialCache = new SerialCacheController();
-        await SerialCache.RemoveCachedSerialNumber(SerialNumber, PartNumber);
-        // return the serial number
+        // get a SerialNumber for this Label and cache it
+        SerialNumber SerialNumber = await GetSerialNumber(Capture.Part, Capture.Process.Serialization);
+        await SerialCacheController.Cache(SerialNumber);
         return SerialNumber;
     }
 }
