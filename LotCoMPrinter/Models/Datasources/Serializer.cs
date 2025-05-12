093706e0bab0d11b1cfb177d6f9c236e9369135a
Mason Ritchason <masonritchason@gmail.com>
2025-05-12 08:45:07 -0400

    refactor(Serializer): change to a single method static class
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/Serializer.cs
+++ LotCoMPrinter/Models/Datasources/Serializer.cs
@@ -3,16 +3,16 @@ namespace LotCoMPrinter.Models.Datasources;
 public static class Serializer 
 {
     /// <summary>
-    /// Retrieves a SerialNumber for Part.
+    /// Assigns a Serial Number to use for a new Print Ticket.
     /// </summary>
+    /// <param name="Process"></param>
     /// <param name="Part"></param>
-    /// <param name="Mode"></param>
     /// <returns></returns>
-    private static async Task<SerialNumber> GetSerialNumber(Part Part, SerializationModes Mode) 
+    public static async Task<SerialNumber> Serialize(Process Process, Part Part) 
     {
         // retrieve a new SerialNumber for the Part
         SerialNumber Number;
-        if (Mode == SerializationModes.JBK) 
+        if (Process.Serialization == SerializationModes.JBK) 
         {
             Number = await new JBKQueue().ConsumeAsync(Part);
         } 
@@ -21,16 +21,5 @@ public static class Serializer
             Number = await new LotQueue().ConsumeAsync(Part);
         }
         return Number;
-    }
-
-    /// <summary>
-    /// Assigns a Serial Number to use for a new Label.
-    /// </summary>
-    /// <param name="Capture">An InterfaceCapture object to use as a source for serialization information.</param>
-    /// <returns>A SerialNumber object.</returns>
-    public static async Task<SerialNumber?> Serialize(InterfaceCapture Capture) 
-    {
-        // get a SerialNumber for this Label
-        return await GetSerialNumber(Capture.Part, Capture.Process.Serialization);
     }
 }
