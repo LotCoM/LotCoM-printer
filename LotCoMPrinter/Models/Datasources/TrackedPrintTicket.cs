28dbd316623301c2cd4683f8af1c80f6ed99e0a8
Mason Ritchason <masonritchason@gmail.com>
2025-05-16 10:38:15 -0400

    feat(TrackedPrintTicket): implement DiscardChanges() and MergeChanges() methods
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/TrackedPrintTicket.cs
+++ LotCoMPrinter/Models/Datasources/TrackedPrintTicket.cs
@@ -9,11 +9,28 @@ public partial class TrackedPrintTicket(PrintTicket Ticket) : ObservableObject()
     /// Does not receive any edits.
     /// </summary>
     [ObservableProperty]
-    public partial PrintTicket Untracked {get; private set;} = Ticket;
+    public partial PrintTicket Untracked { get; private set; } = Ticket;
 
     /// <summary>
     /// A copy of the UntrackedTicket that tracks edits.
     /// </summary>
     [ObservableProperty]
-    public partial PrintTicket Tracked {get; set;} = Ticket;
+    public partial PrintTicket Tracked { get; set; } = Ticket;
+
+    /// <summary>
+    /// Overwrites changes made to Tracked by assigning it the value of Untracked.
+    /// </summary>
+    public void DiscardChanges()
+    {
+        Tracked = Untracked;
+    }
+
+    /// <summary>
+    /// Merges (saves) changes made to Tracked by overwriting Untracked with its value.
+    /// </summary>
+    /// <returns></returns>
+    public void MergeChanges()
+    {
+        Untracked = Tracked;
+    }
 }
