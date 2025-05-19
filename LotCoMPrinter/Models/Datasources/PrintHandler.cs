9d340014bcafc7555e5a9d92fd279b958d685cc1
Mason Ritchason <masonritchason@gmail.com>
2025-05-19 12:04:27 -0400

    refactor(PrintHandler): staticize; refactor to support Label class
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/PrintHandler.cs
+++ LotCoMPrinter/Models/Datasources/PrintHandler.cs
@@ -6,22 +6,21 @@ using LotCoMPrinter.Models.Exceptions;
 namespace LotCoMPrinter.Models.Datasources;
 
 # pragma warning disable CA1416 // Validate platform compatibility
-/// <summary>
-/// Allows the printing of a Label Image.
-/// </summary>
-/// <param name="LabelImage">A Bitmap Label Image.</param>
-public class PrintHandler(Bitmap LabelImage) {
-    // private class attributes
-    private readonly Bitmap _labelImage = LabelImage;
 
+/// <summary>
+/// Allows the printing of a Label.
+/// </summary>
+public static class PrintHandler
+{
     /// <summary>
     /// Loads the Label Image onto the PrintDocument.
     /// </summary>
     /// <param name="Sender"></param>
     /// <param name="e"></param>
-    private void LoadLabelImage(object Sender, PrintPageEventArgs e) {
+    private static void LoadLabelImage(object Sender, PrintPageEventArgs e, Label label)
+    {
         // draw the Label Image onto the PrintDocument Graphic
-        Bitmap Resized = Resizer.ResizeImage(_labelImage, 350, 350);
+        Bitmap Resized = Resizer.ResizeImage(label.GetImage(), 350, 350);
         e.Graphics!.DrawImage(Resized, new System.Drawing.Point(0, 0));
     }
 
@@ -29,37 +28,36 @@ public class PrintHandler(Bitmap LabelImage) {
     /// Prints the Label passed to the PrintHandler.
     /// </summary>
     /// <exception cref="PrintRequestException"></exception>
-    public async Task PrintLabelAsync() {
-        await Task.Run(() => {
-            // create a new PrintDocument to add the Label to
+    public static async Task PrintLabelAsync(Label Label)
+    {
+        await Task.Run(() =>
+        {
+            // create a new PrintDocument, retrieve the Default Printer, and set the Document to use that Printer
             PrintDocument PrintDoc = new PrintDocument();
-            // retrieve the Default Printer Name from a fresh PrinterSettings object
             PrinterSettings Defaults = new PrinterSettings();
             string DefaultPrinterName = Defaults.PrinterName;
-            // set the PrintDocument to print using the default printer
             PrintDoc.PrinterSettings.PrinterName = DefaultPrinterName;
             // add the Label Image Loading logic onto the PrintPage handler
-            try {
-                PrintDoc.PrintPage += LoadLabelImage;
-            // handle exceptions thrown by the addition of the print handler behavior
-            } catch (Exception _ex) {
-                throw new PrintRequestException(
-                    $"The Print Request could not be completed due to the following exception:\n{_ex.Message}."
-                );
+            try
+            {
+                PrintDoc.PrintPage += (sender, e) => LoadLabelImage(sender, e, Label);
+            }
+            catch (Exception _ex)
+            {
+                throw new PrintRequestException($"The Print Request could not be completed due to the following exception:\n{_ex.Message}.");
             }
             // start printing the Document
-            try {
+            try
+            {
                 PrintDoc.Print();
-            // the default printer pulled from the Default PrinterSettings object was invalid
-            } catch (InvalidPrinterException) {
-                throw new PrintRequestException(
-                    "There was an error accessing the Default Printer. Please see management to resolve this issue."
-                );
-            // there was some unexpected printing exception
-            } catch (Exception _ex) {
-                throw new PrintRequestException(
-                    $"The Print Request could not be completed due to the following exception:\n{_ex.Message}."
-                );
+            }
+            catch (InvalidPrinterException)
+            {
+                throw new PrintRequestException("There was an error accessing the Default Printer. Please see management to resolve this issue.");
+            }
+            catch (Exception _ex)
+            {
+                throw new PrintRequestException($"The Print Request could not be completed due to the following exception:\n{_ex.Message}.");
             }
         });
     }
