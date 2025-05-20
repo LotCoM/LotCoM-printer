1c603fd6c5071aabbc30e9cfef6384028d00010d
Mason Ritchason <masonritchason@gmail.com>
2025-05-20 11:56:02 -0400

    feat(HeatNumber): implement HeatNumber class
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Models/Datasources/HeatNumber.cs
@@ -0,0 +1,36 @@
+namespace LotCoMPrinter.Models.Datasources;
+
+/// <summary>
+/// A manufacturing tracing identifier for Basket Labels.
+/// </summary>
+public class HeatNumber
+{
+    /// <summary>
+    /// The absolute lowest digit literal that can be assigned to a Heat Number.
+    /// </summary>
+    private const int MinValue = 1;
+
+    /// <summary>
+    /// The absolute highest digit literal that can be assigned to a Heat Number.
+    /// </summary>
+    private const int MaxValue = 999999999;
+
+    /// <summary>
+    /// The raw literal value of the Heat Number.
+    /// </summary>
+    private readonly int Literal;
+
+    /// <summary>
+    /// Creates a new HeatNumber from Value.
+    /// </summary>
+    /// <param name="Value"></param>
+    public HeatNumber(int Value)
+    {
+        // confirm that Value falls within the allowed literal range
+        if (Value > MaxValue || Value < MinValue)
+        {
+            throw new ArgumentException($"'{Value}' is outside the allowed range of the HeatNumber class.", nameof(Value));
+        }
+        Literal = Value;
+    }
+}
