50099d857b537a6eb72c3b026b05897aad8cfb36
Mason Ritchason <masonritchason@gmail.com>
2025-05-28 08:27:29 -0400

    refactor(Datasources): update database file names
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/JBKQueue.cs
+++ LotCoMPrinter/Models/Datasources/JBKQueue.cs
@@ -4,7 +4,7 @@ namespace LotCoMPrinter.Models.Datasources;
 
 public partial class JBKQueue() : SerialQueue
 (
-    QueuePath: "\\\\144.133.122.1\\Lot Control Management\\Database\\process_control\\serial_queues\\_jbk_queue.json",
+    QueuePath: "\\\\144.133.122.1\\Lot Control Management\\Database\\process_control\\serial_queues\\jbk_queues.json",
     Mode: SerializationMode.JBK,
     Limit: 999
 )
--- LotCoMPrinter/Models/Datasources/LotQueue.cs
+++ LotCoMPrinter/Models/Datasources/LotQueue.cs
@@ -4,7 +4,7 @@ namespace LotCoMPrinter.Models.Datasources;
 
 public partial class LotQueue() : SerialQueue
 (
-    QueuePath: "\\\\144.133.122.1\\Lot Control Management\\Database\\process_control\\serial_queues\\_lot_queue.json",
+    QueuePath: "\\\\144.133.122.1\\Lot Control Management\\Database\\process_control\\serial_queues\\lot_queues.json",
     Mode: SerializationMode.Lot,
     Limit: 999999999
 )
