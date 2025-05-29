dafc258f0a1a8eff47084e9650e4a64daccfb8b8
Mason Ritchason <masonritchason@gmail.com>
2025-05-29 16:17:42 -0400

    feat(Serialization): implement Ping methods to test access to database files
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Services/Serializer.cs
+++ LotCoMPrinter/Models/Services/Serializer.cs
@@ -4,7 +4,7 @@ using LotCoMPrinter.Models.Enums;
 
 namespace LotCoMPrinter.Models.Services;
 
-public static class Serializer 
+public static class Serializer
 {
     /// <summary>
     /// Assigns a Serial Number to use for a new Print Ticket.
@@ -12,18 +12,29 @@ public static class Serializer
     /// <param name="Process"></param>
     /// <param name="Part"></param>
     /// <returns></returns>
-    public static async Task<SerialNumber> Serialize(Process Process, Part Part) 
+    public static async Task<SerialNumber> Serialize(Process Process, Part Part)
     {
         // retrieve a new SerialNumber for the Part
         SerialNumber Number;
-        if (Process.Serialization == SerializationMode.JBK) 
+        if (Process.Serialization == SerializationMode.JBK)
         {
             Number = await new JBKQueue().ConsumeAsync(Part);
-        } 
-        else 
+        }
+        else
         {
             Number = await new LotQueue().ConsumeAsync(Part);
         }
         return Number;
+    }
+
+    /// <summary>
+    /// Pings the class' ability to read and consume numbers from the two Serial Queues.
+    /// </summary>
+    /// <returns></returns>
+    public static bool Ping()
+    {
+        bool JBK = new JBKQueue().Ping();
+        bool Lot = new LotQueue().Ping();
+        return JBK && Lot;
     }
 }
