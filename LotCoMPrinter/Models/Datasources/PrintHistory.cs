931c5de84c2445c532867cfefc0e1d25cfa79ed7
Mason Ritchason <masonritchason@gmail.com>
2025-06-27 14:12:48 -0400

    docs(PrintHistory): comment code
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/PrintHistory.cs
+++ LotCoMPrinter/Models/Datasources/PrintHistory.cs
@@ -8,27 +8,41 @@ using System.Globalization;
 
 namespace LotCoMPrinter.Models.Datasources;
 
+/// <summary>
+/// Provides access to the Print History database.
+/// </summary>
 public static class PrintHistory
 {
-
-    // I want to direct and read correct print process file and save it to a list, ienumberable, or array
+    /// <summary>
+    /// Directory to the Print History database folder.
+    /// </summary>
     private static readonly string PrintFolder = "\\\\144.133.122.1\\Lot Control Management\\Database\\data_tables\\prints";
 
+    /// <summary>
+    /// Opens and reads the Print History for process.
+    /// </summary>
+    /// <param name="process"></param>
+    /// <returns></returns>
+    /// <exception cref="ProcessNameException"></exception>
+    /// <exception cref="DatabaseException"></exception>
     private static IEnumerable<string> Read(Process process)
     {
+        // create Table path and Database set variables
         string TablePath = "";
         IEnumerable<string> DatabaseSet;
+        // attempt to read the Process' Print History datatable
         try
         {
             TablePath = $"{PrintFolder}\\{process.FullName}";
             DatabaseSet = File.ReadAllLines(TablePath);
-
             return DatabaseSet;
         }
+        // the file was not found in the Database
         catch (FileNotFoundException)
         {
             throw new ProcessNameException($"Could not find a table for the Process '{process}'.");
         }
+        // there was some unexpected exception in the reading process
         catch (SystemException _ex)
         {
             throw new DatabaseException
@@ -39,62 +53,74 @@ public static class PrintHistory
         }
     }
 
+    /// <summary>
+    /// Attempts to parse a PrintTicket object from a PrintLog string.
+    /// Uses process as the Process object to reference for PrintTicket information.
+    /// </summary>
+    /// <param name="PrintLog"></param>
+    /// <param name="process"></param>
+    /// <returns></returns>
+    /// <exception cref="ArgumentException"></exception>
     private static PrintTicket ParsePrintTicket(string PrintLog, Process process)
     {
+        // split the PrintLog string into seperate CSV segments
         List<string> SplitPrintLog = PrintLog.Split(",").ToList();
+        // attempt to parse a Part using the Part Number
         string PrintLogPartNumber = SplitPrintLog[1];
         ProcessData processData = new ProcessData();
         Part part = processData.GetProcessPartData(process.FullName, PrintLogPartNumber);
-
+        // retrieve the Process' SerializationMode and use it to create a SerialNumber from the Log
         SerializationMode Mode = process.Serialization;
-
         SerialNumber serialNumber = new SerialNumber(Mode, part, int.Parse(SplitPrintLog[4]));
-
+        // parse the production date
         DateTime PrintLogProductionDate = DateTime.ParseExact(SplitPrintLog[^3], "MM/DD/yyyy-HH:mm:ss", CultureInfo.InvariantCulture);
-
+        // parse initial quantity, shift, and operator values and possible PartialDataSets
         Quantity PrintLogQuanity;
-
         Shift PrintLogShift;
-
         Operator PrintLogOperator;
-
         PartialDataSet? FirstPartialData = null;
         PartialDataSet? SecondPartialData = null;
-        if (SplitPrintLog[3].Contains(":"))
+        // colon indicates the existence of a split basket in the PrintLog
+        if (SplitPrintLog[3].Contains(':'))
         {
+            // split quantity field and save the first, second values
             List<string> SplitQuantity = SplitPrintLog[3].Split(":").ToList();
             PrintLogQuanity = new Quantity(int.Parse(SplitQuantity[0]));
             Quantity FirstPartialQuantity = new Quantity(int.Parse(SplitQuantity[1]));
+            // split shift field and save the first, second values
             List<string> SplitShift = SplitPrintLog[^2].Split(":").ToList();
             PrintLogShift = ShiftExtensions.FromString(SplitShift[0]);
             Shift FirstPartialShift = ShiftExtensions.FromString(SplitShift[1]);
+            // split operator field and save the first, second values
             List<string> SplitOperator = SplitPrintLog[^1].Split(":").ToList();
             PrintLogOperator = new Operator(SplitOperator[0]);
             Operator FirstPartialOperator = new Operator(SplitOperator[1]);
-
+            // construct and save the First PartialDataSet object
             FirstPartialData = new PartialDataSet(FirstPartialQuantity, FirstPartialShift, FirstPartialOperator);
-
+            // length of three in the split quantity field indicates two PartialDataSets
             if (SplitQuantity.Count == 3)
             {
+                // retrieve the third set of values and construct the Second PartialDataSet object
                 Quantity SecondPartialQuantity = new Quantity(int.Parse(SplitQuantity[2]));
                 Shift SecondPartialShift = ShiftExtensions.FromString(SplitShift[2]);
                 Operator SecondPartialOperator = new Operator(SplitOperator[2]);
-
                 SecondPartialData = new PartialDataSet(SecondPartialQuantity, SecondPartialShift, SecondPartialOperator);
             }
-
         }
+        // there is no PartialDataSet information
         else
         {
+            // retrieve values directly from the CSV fields
             PrintLogQuanity = new Quantity(int.Parse(SplitPrintLog[3]));
             PrintLogShift = ShiftExtensions.FromString(SplitPrintLog[^2]);
             PrintLogOperator = new Operator(SplitPrintLog[^1]);
         }
-
-        // Enumerationg over the required fields 
-            VariableFieldSet PrintLogVariableFieldSet = new VariableFieldSet();
+        // enumerate over the required fields and parse them from the CSV fields 
+        VariableFieldSet PrintLogVariableFieldSet = new VariableFieldSet();
         RequiredFields PrintLogRequiredFields = process.RequiredFields;
+        // creates an offset that indicates which variable field to look in
         int NextValue = 0;
+        // parse a JBK # if required
         if (PrintLogRequiredFields.JBKNumber)
         {
             try
@@ -107,7 +133,6 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a JBK Number from the required value {SplitPrintLog[4 + NextValue]}.");
             }
         }
-
         // parse a Lot # if required
         if (PrintLogRequiredFields.LotNumber)
         {
@@ -121,7 +146,6 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a Lot Number from the required value {SplitPrintLog[4 + NextValue]}.");
             }
         }
-
         // parse a Deburr JBK # if required
         if (PrintLogRequiredFields.DeburrJBKNumber)
         {
@@ -135,7 +159,6 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a JBK Number from the required value {SplitPrintLog:[4 + NextValue]}.");
             }
         }
-
         // parse a Die # if required
         if (PrintLogRequiredFields.DieNumber)
         {
@@ -149,7 +172,6 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a Die Number from the required value {SplitPrintLog[4 + NextValue]}.");
             }
         }
-
         // parse a Model # if required
         if (PrintLogRequiredFields.ModelNumber)
         {
@@ -163,7 +185,6 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a Model Number from the required value {SplitPrintLog[4 + NextValue]}.");
             }
         }
-
         // parse a Heat # if required
         if (PrintLogRequiredFields.HeatNumber)
         {
@@ -178,16 +199,22 @@ public static class PrintHistory
                 throw new ArgumentException($"Failed to create a Heat Number from the required value {SplitPrintLog[4 + NextValue]}.");
             }
         }
-
+        // construct and return a new PrintTicket object
         return new PrintTicket(process, part, Mode, serialNumber, PrintLogProductionDate, PrintLogShift, PrintLogQuanity, PrintLogOperator, PrintLogVariableFieldSet, FirstPartialData, SecondPartialData);
     }
 
-    // I want to parse the log in the list and save them as objects to pass to the print job
-    // 4165-CIV-Pivot-Housing-MC,00-T20-532AP-A000-YB1,HOUSING PIVOT*,100:164,008,444,03/10/2025-10:07:43,1:2,ZZZ:XXX
+    /// <summary>
+    /// Parses the Print History for process and returns a List of PrintTicket objects.
+    /// </summary>
+    /// <param name="process"></param>
+    /// <returns></returns>
     public static List<PrintTicket> GetPrintTickets(Process process)
     {
+        // create a list to hold parsed tickets
         List<PrintTicket> printTickets = [];
+        // read the datatable
         IEnumerable<string> DatabaseSet = Read(process);
+        // parse all of the logs to PrintTickets and return them
         foreach (string PrintLog in DatabaseSet)
         {
             printTickets.Add(ParsePrintTicket(PrintLog, process));
