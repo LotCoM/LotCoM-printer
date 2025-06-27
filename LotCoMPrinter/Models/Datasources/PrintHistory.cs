5f09aaa469ccd36b0b1ee83115ac4e29f7e8c979
Mason Ritchason <masonritchason@gmail.com>
2025-06-27 15:56:49 -0400

    refactor(PrintHistory): implement robust exception handling
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

--- LotCoMPrinter/Models/Datasources/PrintHistory.cs
+++ LotCoMPrinter/Models/Datasources/PrintHistory.cs
@@ -94,6 +94,7 @@ public static class PrintHistory
     /// <param name="process"></param>
     /// <returns></returns>
     /// <exception cref="ArgumentException"></exception>
+    /// <exception cref="SystemException"></exception>
     private static PrintTicket ParsePrintTicket(string PrintLog, Process process)
     {
         // split the PrintLog string into seperate CSV segments
@@ -101,42 +102,110 @@ public static class PrintHistory
         // attempt to parse a Part using the Part Number
         string PrintLogPartNumber = SplitPrintLog[1];
         ProcessData processData = new ProcessData();
-        Part part = processData.GetProcessPartData(process.FullName, PrintLogPartNumber);
+        Part part;
+        try
+        {
+            part = processData.GetProcessPartData(process.FullName, PrintLogPartNumber);
+        }
+        catch (ArgumentException)
+        {
+            throw new ArgumentException($"There was no Part '{PrintLogPartNumber}' defined on '{process.FullName}'.");
+        }
+        catch (SystemException)
+        {
+            throw;
+        }
         // retrieve the Process' SerializationMode and use it to create a SerialNumber from the Log
         SerializationMode Mode = process.Serialization;
         SerialNumber serialNumber = new SerialNumber(Mode, part, int.Parse(SplitPrintLog[4]));
         // parse the production date
-        DateTime PrintLogProductionDate = DateTime.ParseExact(SplitPrintLog[^3], "MM/DD/yyyy-HH:mm:ss", CultureInfo.InvariantCulture);
+        DateTime PrintLogProductionDate;
+        try
+        {
+            PrintLogProductionDate = DateTime.ParseExact(SplitPrintLog[^3], "MM/DD/yyyy-HH:mm:ss", CultureInfo.InvariantCulture);
+        }
+        catch (FormatException)
+        {
+            throw new ArgumentException($"Could not parse a date from '{SplitPrintLog[^3]}'.");
+        }
         // parse initial quantity, shift, and operator values and possible PartialDataSets
         Quantity PrintLogQuantity;
         Shift PrintLogShift;
         Operator PrintLogOperator;
         PartialDataSet? FirstPartialData = null;
+        Quantity FirstPartialQuantity;
+        Shift FirstPartialShift;
+        Operator FirstPartialOperator;
         PartialDataSet? SecondPartialData = null;
+        Quantity SecondPartialQuantity;
+        Shift SecondPartialShift;
+        Operator SecondPartialOperator;
         // colon indicates the existence of a split basket in the PrintLog
         if (SplitPrintLog[3].Contains(':'))
         {
             // split quantity field and save the first, second values
             List<string> SplitQuantity = SplitPrintLog[3].Split(":").ToList();
-            PrintLogQuantity = new Quantity(int.Parse(SplitQuantity[0]));
-            Quantity FirstPartialQuantity = new Quantity(int.Parse(SplitQuantity[1]));
+            try
+            {
+                PrintLogQuantity = new Quantity(int.Parse(SplitQuantity[0]));
+                FirstPartialQuantity = new Quantity(int.Parse(SplitQuantity[1]));
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse integer Quantities from '{SplitPrintLog[3]}'.");
+            }
             // split shift field and save the first, second values
             List<string> SplitShift = SplitPrintLog[^2].Split(":").ToList();
-            PrintLogShift = ShiftExtensions.FromString(SplitShift[0]);
-            Shift FirstPartialShift = ShiftExtensions.FromString(SplitShift[1]);
+            try
+            {
+                PrintLogShift = ShiftExtensions.FromString(SplitShift[0]);
+                FirstPartialShift = ShiftExtensions.FromString(SplitShift[1]);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse Shifts from '{SplitPrintLog[^2]}'.");
+            }
             // split operator field and save the first, second values
             List<string> SplitOperator = SplitPrintLog[^1].Split(":").ToList();
-            PrintLogOperator = new Operator(SplitOperator[0]);
-            Operator FirstPartialOperator = new Operator(SplitOperator[1]);
+            try
+            {
+                PrintLogOperator = new Operator(SplitOperator[0]);
+                FirstPartialOperator = new Operator(SplitOperator[1]);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse string Operators from '{SplitPrintLog[^1]}'.");
+            }
             // construct and save the First PartialDataSet object
             FirstPartialData = new PartialDataSet(FirstPartialQuantity, FirstPartialShift, FirstPartialOperator);
             // length of three in the split quantity field indicates two PartialDataSets
             if (SplitQuantity.Count == 3)
             {
                 // retrieve the third set of values and construct the Second PartialDataSet object
-                Quantity SecondPartialQuantity = new Quantity(int.Parse(SplitQuantity[2]));
-                Shift SecondPartialShift = ShiftExtensions.FromString(SplitShift[2]);
-                Operator SecondPartialOperator = new Operator(SplitOperator[2]);
+                try
+                {
+                    SecondPartialQuantity = new Quantity(int.Parse(SplitQuantity[2]));
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"Could not parse int Quantity from '{SplitQuantity[2]}'.");
+                }
+                try
+                {
+                    SecondPartialShift = ShiftExtensions.FromString(SplitShift[2]);
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"Could not parse Shift from '{SplitShift[2]}'.");
+                }
+                try
+                {
+                    SecondPartialOperator = new Operator(SplitOperator[2]);
+                }
+                catch (FormatException)
+                {
+                    throw new ArgumentException($"Could not parse string Operator from '{SplitOperator[2]}'.");
+                }
                 SecondPartialData = new PartialDataSet(SecondPartialQuantity, SecondPartialShift, SecondPartialOperator);
             }
         }
@@ -144,9 +213,30 @@ public static class PrintHistory
         else
         {
             // retrieve values directly from the CSV fields
-            PrintLogQuantity = new Quantity(int.Parse(SplitPrintLog[3]));
-            PrintLogShift = ShiftExtensions.FromString(SplitPrintLog[^2]);
-            PrintLogOperator = new Operator(SplitPrintLog[^1]);
+            try
+            {
+                PrintLogQuantity = new Quantity(int.Parse(SplitPrintLog[3]));
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse int Quantity from '{SplitPrintLog[3]}'.");
+            }
+            try
+            {
+                PrintLogShift = ShiftExtensions.FromString(SplitPrintLog[^2]);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse Shift from '{SplitPrintLog[^2]}'.");
+            }
+            try
+            {
+                PrintLogOperator = new Operator(SplitPrintLog[^1]);
+            }
+            catch (FormatException)
+            {
+                throw new ArgumentException($"Could not parse string Operator from '{SplitPrintLog[^1]}'.");
+            }
         }
         // enumerate over the required fields and parse them from the CSV fields 
         VariableFieldSet PrintLogVariableFieldSet = new VariableFieldSet();
@@ -249,7 +339,14 @@ public static class PrintHistory
         List<PrintTicket> printTickets = [];
         foreach (string PrintLog in DatabaseSet)
         {
-            printTickets.Add(ParsePrintTicket(PrintLog, process));
+            try
+            {
+                printTickets.Add(ParsePrintTicket(PrintLog, process));
+            }
+            catch
+            {
+                continue;
+            }
         }
         return printTickets;
     }
@@ -264,11 +361,34 @@ public static class PrintHistory
         // read the datatable
         IEnumerable<string> DatabaseSet = await ReadAsync(process);
         // parse all of the logs to PrintTickets and return them
-        List<PrintTicket> printTickets = [];
+        List<PrintTicket?> printTickets = [];
         printTickets = DatabaseSet
-            .Select(x => ParsePrintTicket(x, process))
+            .Select(x =>
+            {
+                try
+                {
+                    return ParsePrintTicket(x, process);
+                }
+                // sets faulting PrintTicket parses to null
+                catch
+                {
+                    return null;
+                }
+            })
             .ToList();
-        return printTickets;
+        // remove nulls
+        printTickets = printTickets
+            .Where(x => x is not null)
+            .ToList();
+        // return empty list if null; else return list with all nulls removed
+        if (printTickets is null)
+        {
+            return [];
+        }
+        else
+        {
+            return printTickets!;
+        }
     }
 }   
 
