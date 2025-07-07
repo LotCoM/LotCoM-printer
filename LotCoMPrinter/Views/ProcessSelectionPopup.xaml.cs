1e1ebf83510ce0c3ab0174f822a9880da1efca07
Mason Ritchason <masonritchason@gmail.com>
2025-07-07 15:35:49 -0400

    feat(ProcessSelectionPopup): implement ProcessSelectionPopup View
    Signed-off-by: Mason Ritchason <masonritchason@gmail.com>

---
+++ LotCoMPrinter/Views/ProcessSelectionPopup.xaml.cs
@@ -0,0 +1,55 @@
+using CommunityToolkit.Maui.Views;
+using LotCom.Database;
+using LotCom.Types;
+
+namespace LotCoMPrinter.Views;
+
+public partial class ProcessSelectionPopup : Popup
+{
+    private Process? _selectedProcess;
+    /// <summary>
+    /// Controls the currently selected Process in the ProcessCollectionView.
+    /// </summary>
+    public Process? SelectedProcess
+    {
+        get { return _selectedProcess; }
+        set
+        {
+            _selectedProcess = value;
+            OnPropertyChanged(nameof(_selectedProcess));
+            OnPropertyChanged(nameof(SelectedProcess));
+        }
+    }
+
+    /// <summary>
+    /// Handler for the Clicked event from the ConfirmButton control.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void OnConfirmButtonClicked(object sender, EventArgs e)
+    {
+        CancellationTokenSource TokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await CloseAsync(SelectedProcess, TokenSource.Token);
+    }
+
+    /// <summary>
+    /// Handler for the Clicked event from the CancelButton control.
+    /// </summary>
+    /// <param name="sender"></param>
+    /// <param name="e"></param>
+    private async void OnCancelButtonClicked(object sender, EventArgs e)
+    {
+        CancellationTokenSource TokenSource = new CancellationTokenSource(TimeSpan.FromSeconds(5));
+        await CloseAsync(null, TokenSource.Token);
+    }
+
+    /// <summary>
+    /// Create a new Process Selection Popup window.
+    /// </summary>
+    public ProcessSelectionPopup()
+    {
+        InitializeComponent();
+        BindingContext = this;
+        ProcessCollectionView.ItemsSource = new ProcessData().GetAllProcesses();
+    }
+}
